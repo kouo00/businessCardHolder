d28998a8d7a1328aa67e5b78196eefdb38a39d28
kouo00 <denfort363@gmail.com>
2021-03-29 23:05:41 +0200

    [FIX] strict types

--- app/Components/BusinessCardControl.php
+++ app/Components/BusinessCardControl.php
@@ -1,14 +1,15 @@
-<?php
+<?php declare(strict_types=1);
 
 
 namespace App\Components;
 
 use Nette\Application\UI\Control;
+use Nette\Database\Table\ActiveRow;
 
 
 class BusinessCardControl extends Control
 {
-	public function render($values)
+	public function render(ActiveRow $values)
 	{
 		$this->template->setFile(__DIR__ . '/businessCard.latte');
 		$this->template->values = $values;
