3cbe93e2e19a0a1e4e377237d748730038a736de
kouo00 <denfort363@gmail.com>
2021-04-03 10:09:36 +0200

    [UPD] Multiplier

--- app/Components/BusinessCardControl.php
+++ app/Components/BusinessCardControl.php
@@ -5,40 +5,22 @@ namespace App\Components;
 
 use App\Model\BusinessCardManager;
 use Nette\Application\UI\Control;
+use Nette\Database\Table\ActiveRow;
 
 
 class BusinessCardControl extends Control
 {
-	private BusinessCardManager $businessCardManager;
+	private ActiveRow $businessCard;
 
-	public function __construct(BusinessCardManager $businessCardManager)
+	public function __construct(ActiveRow $businessCard)
 	{
-		$this->businessCardManager = $businessCardManager;
+		$this->businessCard = $businessCard;
 	}
 
 	public function render()
 	{
 		$this->template->setFile(__DIR__ . '/businessCard.latte');
-
-		foreach ($this->getValues() as $businessCard){
-			$this->template->values = $businessCard;
+			$this->template->values = $this->businessCard;
 			$this->template->render();
-		}
-	}
-
-	public function handleEditBusinessCard($id): void
-	{
-		$this->getPresenter()->redirect('BusinessCard:edit', ['id' => $id]);
-	}
-
-	public function handleDeleteBusinessCard($id): void
-	{
-		$this->businessCardManager->delete($id);
-		$this->flashMessage('Vizitka smazána');
-		$this->redirect('this');
-	}
-
-	private function getValues(){
-		return $this->businessCardManager->getRows([BusinessCardManager::COLUMN_USERS_ID => $this->getPresenter()->getUser()->getId()]);
 	}
 }
--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -5,7 +5,10 @@ namespace App\Presenters;
 
 use App\Components\BusinessCardControl;
 use App\Model\BusinessCardManager;
+use Nette\Application\UI\Form;
+use Nette\Application\UI\Multiplier;
 use Nette\ComponentModel\IComponent;
+use Nette\Database\Table\ActiveRow;
 
 
 final class HomepagePresenter extends BasePresenter
@@ -23,13 +26,32 @@ final class HomepagePresenter extends BasePresenter
 		parent::startup();
 	}
 
+	public function renderDefault()
+	{
+		$this->template->businessCards = $this->businessCardManager->getRows([BusinessCardManager::COLUMN_USERS_ID => $this->getUser()->getId()]);
+	}
+
 	public function handleNewBusinessCard(): void
 	{
 		$this->redirect('BusinessCard:New');
 	}
 
-	public function createComponentBusinessCards(): IComponent
+	public function handleEditBusinessCard($id): void
 	{
-		return new BusinessCardControl($this->businessCardManager);
+		$this->redirect('BusinessCard:edit', ['id' => $id]);
+	}
+
+	public function handleDeleteBusinessCard($id): void
+	{
+		$this->businessCardManager->delete($id);
+		$this->flashMessage('Vizitka smazána');
+		$this->redirect('this');
+	}
+
+	public function createComponentBusinessCards(): Multiplier
+	{
+		return new Multiplier(function ($id) {
+			return new BusinessCardControl($this->template->businessCards[$id]);
+		});
 	}
 }
